079fd187e451bd7f64a23c1f9890c3021804ccd3
moritanian <pickit2express@gmail.com>
2025-10-28 07:10:02 +0900

    Add Python pack and disable LuaRocks builds

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.vscode-nvim" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrotheme", "habamax" } },
   ui = { backdrop = 100 },
+  rocks = { enabled = false },
   performance = {
     rtp = {
       -- disable some rtp plugins, add more to your liking
